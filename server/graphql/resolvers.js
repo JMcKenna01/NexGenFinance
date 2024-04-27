f95ea1c67ea8cb84783a97c53a4bb89f01c9bf20
jmckenna01 <jmckenna2026@gmail.com>
2024-04-27 13:16:32 -0400

    added models index.js

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { User, Transaction, Account, Budget, Investment } = require('../../models')
+const { Account, Budget, Investment, Transaction, User } = require('../../models');
 
 // Helper function to create a JWT token
 const createToken = (user, expiresIn) => {
---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    Account: require('./Account'),
+    Budget: require('./Budget'),
+    Investment: require('./Investment'),
+    Transaction: require('./Transaction'),
+    User: require('./user')
+};
