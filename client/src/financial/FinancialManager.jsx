2017d739f57aef9ac72ae3e72bed3fdc8a23b211
jmckenna01 <jmckenna2026@gmail.com>
2024-06-28 17:11:34 -0400

    added financial manager component

---
+++ client/src/financial/FinancialManager.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './FinancialManager.module.css';
+
+const FinancialManager = () => {
+  return (
+    <div className="financial-manager">
+      <h1>Financial Manager</h1>
+      {/* Add your component code here */}
+    </div>
+  );
+};
+
+export default FinancialManager;
